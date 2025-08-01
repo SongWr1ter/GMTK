50867397eccaabf64400a54c4f767e61e3507c62
songbkb <swzbkb@163.com>
2025-08-01 09:44:22 +0800

    init 2

---
+++ Assets/Scripts/AbnormalObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbnormalObject : MonoBehaviour
+{
+    //两大类别:贴图不同 and 交互不同
+    //异常数据的存储——异常列表
+    //贴图:初始化时，从数据列表中选择特定贴图并显示 (不同的初始化方法
+    //交互:被交互时，逻辑可能不变，但是展示的数据会变 (可能不同的交互方法
+    //展示的数据:UI文本、UI图片
+    
+    //string audio sprite function
+    
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
